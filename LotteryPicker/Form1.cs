3603ab2e4c291ce08804f9cfc055059983d3630c
Adam Wingert <adamwing415@ed.amdsb.ca>
2020-05-26 16:42:26 -0400

    fini

--- LotteryPicker/Form1.cs
+++ LotteryPicker/Form1.cs
@@ -24,23 +24,27 @@ namespace LotteryPicker
 
         private void generateButton_Click(object sender, EventArgs e)
         {
+            //resets text
             outputLabel.ResetText();
             Refresh();
 
-            int maxNum = 9;
-
+            //variable for number of numbers
+            int maxNum;
+            //sets number of numbers
             maxNum = Convert.ToInt32(NumberPicker.SelectedItem);
             
-
+            //creates list for numbers
             List <int> lotteryList = new List <int>();
 
-
+            //adds first nadom number
             lotteryList.Add(numGen.Next(1, MAX));
 
+            //adds as many random numbers as there needs to be
             for (int i = 1; i < maxNum; i++)
             {
                 int nextNum = numGen.Next(1, MAX);
 
+                //if there are duplicate numbers, it re-randomizes them
                 while (lotteryList.Contains(nextNum))
                 {
                     nextNum = numGen.Next(1, MAX);
@@ -48,9 +52,10 @@ namespace LotteryPicker
                 lotteryList.Add(nextNum);
             }
 
-
+            //sorts list for output
             lotteryList.Sort();
 
+            //outputs final list
             for (int i = 0; i < maxNum; i++)
             {
                 outputLabel.Text += lotteryList[i] + " ";
